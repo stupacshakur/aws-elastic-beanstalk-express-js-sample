2fdea259f61778e7ccc04dc3cd3ec31d465dbe3b
Stuart Weiss <stupac@gmail.com>
2021-03-17 07:31:18 -0700

    decided to be polite

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Don\'t suck!'));
+app.get('/', (req, res) => res.send('Please Don\'t suck!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
